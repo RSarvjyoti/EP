91ccd8f680b90837e30d049f83eb815f18550727
RSarvjyoti <sarvjyoti64@gmail.com>
2025-02-20 10:45:24 +0530

    day02 authentication

--- Day1/index.js
+++ Day1/index.js
@@ -4,11 +4,15 @@ const connectDB = require("./src/config/db");
 config();
 const CORS = require("cors");
 const Task = require("./src/models/TaskModel");
-
+const User = require("./src/models/user.model");
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
 const app = express();
 const PORT = process.env.PORT || 8090
 const MONGO_URL = process.env.MONGO_DB
+const JWT_SECRET = process.env.JWT_SECRET
+
 app.use(express.json());
 app.use(CORS());
 
@@ -75,6 +79,65 @@ app.patch('/tasks/:id', async (req, res) => {
         })
     }catch(err) {
         res.send({
+            message : err
+        })
+    }
+});
+
+// ========== register route ===========
+app.post('/register',async (req, res) => {
+    const {username, email, password} = req.body;
+    try{
+        const users = User.findOne({email});
+        if(users) {
+            res.status(400).send({
+                message : "User already register try to login"
+            })
+        }
+        const hashpassword = await bcrypt.hash(password, 10);
+        const user = await User.create({
+            username,
+            email,
+            password : hashpassword
+        })
+
+        res.status(200).send({
+            message : "User Created",
+            user
+        })
+    }catch(err) {
+        res.status(500).send({
+            message : err
+        })
+    }
+})
+
+
+// ======== Login route ========
+app.post('/login', async(req, res) => {
+    const {email, password} = req.body;
+    try{
+        const user = await User.findOne({email});
+
+        if(!user) {
+            res.status(400).send({
+                message : "User not register, please register first"
+            })
+        }
+
+        const match = await bcrypt.compare(password, user.password);
+
+        if(!match) {
+            res.status(400).send({
+                message : "Invailid cradencial"
+            })
+        }
+
+        var token = jwt.sign({ userId: user._id, email: user.email  }, JWT_SECRET, { expiresIn: "1h" });
+        res.status(200).send({ message: "Login successful", token });
+
+    }catch(err) {
+        res.status(500).send({
             message : err
         })
     }
---
+++ Day1/src/models/user.model.js
@@ -0,0 +1,12 @@
+const {Schema, model} = require("mongoose");
+
+const userSchema = new Schema({
+    username : {type : String, required : true, unique : true},
+    email : {type : String, required : true, unique : true},
+    password : {type : String, required : true, min : 3}
+})
+
+
+const User = model('users', userSchema);
+
+module.exports = User;
